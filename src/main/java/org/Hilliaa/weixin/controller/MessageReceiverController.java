ca1e4cf1e02175fe5796ba77f3b398157986bc85
Hillia <272715237@qq.com>
2019-07-04 15:21:29 +0800

    接收微信公众号平台发送给我们的消息

--- src/main/java/org/Hilliaa/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/Hilliaa/weixin/controller/MessageReceiverController.java
@@ -1,6 +1,10 @@
 package org.Hilliaa.weixin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hillia/weixin/receiver")
 public class MessageReceiverController {
+	
+	private static final Logger LOG=LoggerFactory.getLogger(MessageReceiverController.class);
 	
 	@GetMapping
 	public String echo(
@@ -17,5 +23,15 @@ public class MessageReceiverController {
 			@RequestParam("echostr") String echostr
 			) {
 		return echostr;
+	}
+	
+	@PostMapping
+	public String onMessage(
+			@RequestParam("signature") String signature,
+			@RequestParam("timestamp") String timestamp,
+			@RequestParam("nonce") String nonce,
+			@RequestBody String xml) {
+		LOG.trace("收到的信息原文:\n{}\n-------------------------",xml);
+		return "success";
 	}
 }
